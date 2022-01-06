0d0c2879a57099a643d0236858413651ba34f6e2
shitcoding <shitcoder@pm.me>
2022-01-06 23:29:20 +0300

    edit tests to read expected output from fixture

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,18 +1,21 @@
 import json
+import os
 from gendiff import generate_diff
 
 
-EXPECTED_PLAIN_JSON_DIFF = """{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}"""
+def get_fixture_path(fixture_filename):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(current_dir, 'fixtures', fixture_filename)
+
+
+def read_file(file_path):
+    with open(file_path, 'r') as f:
+        data = f.read()
+    return data
 
 
 def test_plain_json():
-    data1 = json.load(open('./tests/fixtures/plain_file1.json'))
-    data2 = json.load(open('./tests/fixtures/plain_file2.json'))
-    assert generate_diff(data1, data2) == EXPECTED_PLAIN_JSON_DIFF
+    data1 = json.load(open(get_fixture_path('plain1.json')))
+    data2 = json.load(open(get_fixture_path('plain2.json')))
+    expected = read_file(get_fixture_path('plain_json_expected.txt')).rstrip()
+    assert generate_diff(data1, data2) == expected
