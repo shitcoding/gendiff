b9c7aa67f0d56ef4c23dd50b71f9db6dc8e46cf9
shitcoding <shitcoder@pm.me>
2022-01-14 00:45:07 +0300

    separate diff data and diff output formatting logic

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,22 +2,29 @@
 
 
 def generate_diff(dict1, dict2):
-    """
-    Return diff string between 2 dictionaries.
-
-    Returns:
-        str
-    """
-    diff_items = []
+    diff = {}
     keys = dict1.keys() | dict2.keys()
-    for key in sorted(keys):
+    for key in keys:
         if key not in dict1:
-            diff_items.append('  + {0}: {1}'.format(key, dict2[key]))
+            # key added to dict2
+            value = dict2[key]
+            diff[key] = ("added", value)
         elif key not in dict2:
-            diff_items.append('  - {0}: {1}'.format(key, dict1[key]))
+            # key removed
+            value = dict1[key]
+            diff[key] = ("removed", value)
         elif dict1[key] == dict2[key]:
-            diff_items.append('    {0}: {1}'.format(key, dict1[key]))
+            # key and value are unchanged
+            value = dict1[key]
+            diff[key] = ("unchanged", value)
         else:
-            diff_items.append('  - {0}: {1}'.format(key, dict1[key]))
-            diff_items.append('  + {0}: {1}'.format(key, dict2[key]))
-    return '\n'.join(['{'] + diff_items + ['}']).lower()
+            # value has changed
+            old_value, new_value = dict1[key], dict2[key]
+            # both values are nested, generate diff for them
+            if isinstance(old_value, dict) and isinstance(new_value, dict):
+                value = generate_diff(old_value, new_value)
+                diff[key] = ("nested", value)
+            # one or both values are plain, no need to generate diff
+            else:
+                diff[key] = ("changed", (old_value, new_value))
+    return diff
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,13 +1,14 @@
 """Gendiff script."""
 
 from gendiff import cli, generate_diff, parser
+from gendiff.views import stylish
 
 
 def main():
     path1, path2 = cli.args.first_file, cli.args.second_file
     dict1 = parser.parse(path1)
     dict2 = parser.parse(path2)
-    print(generate_diff(dict1, dict2))
+    print(stylish.format_diff(generate_diff(dict1, dict2)))
 
 
 if __name__ == '__main__':
