7b151e86d10645efb189cc500121e1a56df9825d
shitcoding <shitcoder@pm.me>
2022-01-15 00:30:40 +0300

    add choice of diff output formatter via cli arg

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -7,6 +7,10 @@ parser = argparse.ArgumentParser(prog='gendiff',
                                  description='Generate diff')
 parser.add_argument('first_file')   # Path to first file
 parser.add_argument('second_file')  # Path to second file
-parser.add_argument('-f', '--format', help='set format of output')
+parser.add_argument('-f', '--format',
+                    choices=['stylish', 'plain'],
+                    default='stylish',
+                    help='set format of output'
+                    )
 
 args = parser.parse_args()
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,14 +1,21 @@
 """Gendiff script."""
 
 from gendiff import cli, generate_diff, parser
-from gendiff.views import stylish
+from gendiff.views import plain, stylish
+
+
+FORMATTERS = {
+    'stylish': stylish,
+    'plain': plain,
+}
 
 
 def main():
     path1, path2 = cli.args.first_file, cli.args.second_file
+    formatter = FORMATTERS[cli.args.format]
     dict1 = parser.parse(path1)
     dict2 = parser.parse(path2)
-    print(stylish.format_diff(generate_diff(dict1, dict2)))
+    print(formatter.format_diff(generate_diff(dict1, dict2)))
 
 
 if __name__ == '__main__':
