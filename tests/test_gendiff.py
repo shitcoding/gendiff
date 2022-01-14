ab232a31d5b97b9706860038fd6c57696405c84c
shitcoding <shitcoder@pm.me>
2022-01-15 00:29:35 +0300

    add tests for plain formatter

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,7 +2,7 @@ import json
 import os
 import yaml
 from gendiff import generate_diff
-from gendiff.views import stylish
+from gendiff.views import stylish, plain
 
 
 def get_fixture_path(fixture_filename):
@@ -29,25 +29,38 @@ def get_expected_output(output_fixture_file_name):
 
 
 # Formatter: stylish
-def test_plain_json_stylish():
+def test_plain_json_stylish_formatter():
     data1, data2 = get_json_data('plain1.json'), get_json_data('plain2.json')
-    expected = get_expected_output('plain_expected.txt')
+    expected = get_expected_output('plain_expected_stylish.txt')
     assert stylish.format_diff(generate_diff(data1, data2)) == expected
 
 
-def test_plain_yaml_stylish():
+def test_plain_yaml_stylish_formatter():
     data1, data2 = get_yaml_data('plain1.yaml'), get_yaml_data('plain2.yaml')
-    expected = get_expected_output('plain_expected.txt')
+    expected = get_expected_output('plain_expected_stylish.txt')
     assert stylish.format_diff(generate_diff(data1, data2)) == expected
 
 
-def test_nested_json_stylish():
+def test_nested_json_stylish_formatter():
     data1, data2 = get_json_data('nested1.json'), get_json_data('nested2.json')
-    expected = get_expected_output('nested_expected.txt')
+    expected = get_expected_output('nested_expected_stylish.txt')
     assert stylish.format_diff(generate_diff(data1, data2)) == expected
 
 
-def test_nested_yaml_stylish():
+def test_nested_yaml_stylish_formatter():
     data1, data2 = get_yaml_data('nested1.yaml'), get_yaml_data('nested2.yaml')
-    expected = get_expected_output('nested_expected.txt')
+    expected = get_expected_output('nested_expected_stylish.txt')
     assert stylish.format_diff(generate_diff(data1, data2)) == expected
+
+
+# Formatter: plain
+def test_nested_json_plain_formatter():
+    data1, data2 = get_json_data('nested1.json'), get_json_data('nested2.json')
+    expected = get_expected_output('nested_expected_plain.txt')
+    assert plain.format_diff(generate_diff(data1, data2)) == expected
+
+
+def test_nested_yaml_plain_formatter():
+    data1, data2 = get_yaml_data('nested1.yaml'), get_yaml_data('nested2.yaml')
+    expected = get_expected_output('nested_expected_plain.txt')
+    assert plain.format_diff(generate_diff(data1, data2)) == expected
