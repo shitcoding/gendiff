02d8872dd7067a7e80a9face3a4116bfcdfe250f
shitcoding <shitcoder@pm.me>
2022-01-15 20:58:07 +0300

    rewrite tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,9 +1,5 @@
-import json
 import os
-import yaml
 from gendiff import generate_diff
-from gendiff.views import stylish, plain
-import gendiff.views.json as json_formatter
 
 
 def get_fixture_path(fixture_filename):
@@ -17,64 +13,65 @@ def read_file(file_path):
     return data
 
 
-def get_json_data(json_file_name):
-    return json.load(open(get_fixture_path(json_file_name)))
-
-
-def get_yaml_data(yaml_file_name):
-    return yaml.safe_load(open(get_fixture_path(yaml_file_name)))
-
-
 def get_expected_output(output_fixture_file_name):
     return read_file(get_fixture_path(output_fixture_file_name)).rstrip()
 
 
 # Formatter: stylish
 def test_plain_json_stylish_formatter():
-    data1, data2 = get_json_data('plain1.json'), get_json_data('plain2.json')
+    path1 = get_fixture_path('plain1.json')
+    path2 = get_fixture_path('plain2.json')
     expected = get_expected_output('plain_expected_stylish.txt')
-    assert stylish.format_diff(generate_diff(data1, data2)) == expected
+    assert generate_diff(path1, path2) == expected
 
 
 def test_plain_yaml_stylish_formatter():
-    data1, data2 = get_yaml_data('plain1.yaml'), get_yaml_data('plain2.yaml')
+    path1 = get_fixture_path('plain1.yaml')
+    path2 = get_fixture_path('plain2.yaml')
     expected = get_expected_output('plain_expected_stylish.txt')
-    assert stylish.format_diff(generate_diff(data1, data2)) == expected
+    assert generate_diff(path1, path2) == expected
 
 
 def test_nested_json_stylish_formatter():
-    data1, data2 = get_json_data('nested1.json'), get_json_data('nested2.json')
+    path1 = get_fixture_path('nested1.json')
+    path2 = get_fixture_path('nested2.json')
     expected = get_expected_output('nested_expected_stylish.txt')
-    assert stylish.format_diff(generate_diff(data1, data2)) == expected
+    assert generate_diff(path1, path2) == expected
 
 
 def test_nested_yaml_stylish_formatter():
-    data1, data2 = get_yaml_data('nested1.yaml'), get_yaml_data('nested2.yaml')
+    path1 = get_fixture_path('nested1.yaml')
+    path2 = get_fixture_path('nested2.yaml')
     expected = get_expected_output('nested_expected_stylish.txt')
-    assert stylish.format_diff(generate_diff(data1, data2)) == expected
+    assert generate_diff(path1, path2) == expected
 
 
 # Formatter: plain
 def test_nested_json_plain_formatter():
-    data1, data2 = get_json_data('nested1.json'), get_json_data('nested2.json')
+    path1 = get_fixture_path('nested1.json')
+    path2 = get_fixture_path('nested2.json')
     expected = get_expected_output('nested_expected_plain.txt')
-    assert plain.format_diff(generate_diff(data1, data2)) == expected
+    assert generate_diff(path1, path2, formatter='plain') == expected
 
 
 def test_nested_yaml_plain_formatter():
-    data1, data2 = get_yaml_data('nested1.yaml'), get_yaml_data('nested2.yaml')
+    path1 = get_fixture_path('nested1.yaml')
+    path2 = get_fixture_path('nested2.yaml')
     expected = get_expected_output('nested_expected_plain.txt')
-    assert plain.format_diff(generate_diff(data1, data2)) == expected
+    assert generate_diff(path1, path2, formatter='plain') == expected
 
 
 # Formatter: Json
 def test_nested_json_json_formatter():
-    data1, data2 = get_json_data('nested1.json'), get_json_data('nested2.json')
+    path1 = get_fixture_path('nested1.json')
+    path2 = get_fixture_path('nested2.json')
     expected = get_expected_output('nested_expected_json.txt')
-    assert json_formatter.format_diff(generate_diff(data1, data2)) == expected
+    assert generate_diff(path1, path2, formatter='json') == expected
 
 
 def test_nested_yaml_json_formatter():
-    data1, data2 = get_yaml_data('nested1.yaml'), get_yaml_data('nested2.yaml')
+    path1 = get_fixture_path('nested1.yaml')
+    path2 = get_fixture_path('nested2.yaml')
     expected = get_expected_output('nested_expected_json.txt')
-    assert json_formatter.format_diff(generate_diff(data1, data2)) == expected
+    assert generate_diff(path1, path2, formatter='json') == expected
+
