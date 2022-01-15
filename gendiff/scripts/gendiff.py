9bcbde031b516377b0c3a8d7f10e351146a8f39f
shitcoding <shitcoder@pm.me>
2022-01-15 19:48:05 +0300

    refactoring

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -14,7 +14,7 @@ FORMATTERS = {
 def main():
     path1, path2 = cli.args.first_file, cli.args.second_file
     formatter = FORMATTERS[cli.args.format]
-    print(formatter.format_diff(generate_diff(path1, path2)))
+    return formatter.format_diff(generate_diff(path1, path2))
 
 
 if __name__ == '__main__':
