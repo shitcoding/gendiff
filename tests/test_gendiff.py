261fb4a05b8fa20c2721e73600469c4529b10162
shitcoding <shitcoder@pm.me>
2022-01-31 16:46:17 +0300

    use pytese.park.parametrize for fixtures in tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,76 +1,35 @@
 import os
 from gendiff import generate_diff
+import pytest
 
 
-def get_fixture_path(fixture_filename):
+def get_path(fixture_filename):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(current_dir, 'fixtures', fixture_filename)
 
 
-def read_file(file_path):
-    with open(file_path, 'r') as f:
+def read_fixture(filename):
+    path = get_path(filename)
+    with open(path, 'r') as f:
         data = f.read()
-    return data
+        return data.rstrip()
 
 
-def get_expected_output(output_fixture_file_name):
-    return read_file(get_fixture_path(output_fixture_file_name)).rstrip()
+TEST_CASES = [
+    ("plain1.json", "plain2.json", "stylish", "plain_expected_stylish.txt"),
+    ("plain1.yaml", "plain2.yaml", "stylish", "plain_expected_stylish.txt"),
+    ("nested1.json", "nested2.json", "stylish", "nested_expected_stylish.txt"),
+    ("nested1.yaml", "nested2.yaml", "stylish", "nested_expected_stylish.txt"),
+    ("nested1.json", "nested2.json", "plain", "nested_expected_plain.txt"),
+    ("nested1.yaml", "nested2.yaml", "plain", "nested_expected_plain.txt"),
+    ("nested1.json", "nested2.json", "json", "nested_expected_json.txt"),
+    ("nested1.yaml", "nested2.yaml", "json", "nested_expected_json.txt"),
+]
 
 
-# Formatter: stylish
-def test_plain_json_stylish_formatter():
-    path1 = get_fixture_path('plain1.json')
-    path2 = get_fixture_path('plain2.json')
-    expected = get_expected_output('plain_expected_stylish.txt')
-    assert generate_diff(path1, path2) == expected
+@pytest.mark.parametrize("file1, file2, formatter, expected", TEST_CASES)
+def test_gendiff(file1, file2, formatter, expected):
+    path1 = get_path(file1)
+    path2 = get_path(file2)
+    assert generate_diff(path1, path2, formatter) == read_fixture(expected)
 
-
-def test_plain_yaml_stylish_formatter():
-    path1 = get_fixture_path('plain1.yaml')
-    path2 = get_fixture_path('plain2.yaml')
-    expected = get_expected_output('plain_expected_stylish.txt')
-    assert generate_diff(path1, path2) == expected
-
-
-def test_nested_json_stylish_formatter():
-    path1 = get_fixture_path('nested1.json')
-    path2 = get_fixture_path('nested2.json')
-    expected = get_expected_output('nested_expected_stylish.txt')
-    assert generate_diff(path1, path2) == expected
-
-
-def test_nested_yaml_stylish_formatter():
-    path1 = get_fixture_path('nested1.yaml')
-    path2 = get_fixture_path('nested2.yaml')
-    expected = get_expected_output('nested_expected_stylish.txt')
-    assert generate_diff(path1, path2) == expected
-
-
-# Formatter: plain
-def test_nested_json_plain_formatter():
-    path1 = get_fixture_path('nested1.json')
-    path2 = get_fixture_path('nested2.json')
-    expected = get_expected_output('nested_expected_plain.txt')
-    assert generate_diff(path1, path2, formatter='plain') == expected
-
-
-def test_nested_yaml_plain_formatter():
-    path1 = get_fixture_path('nested1.yaml')
-    path2 = get_fixture_path('nested2.yaml')
-    expected = get_expected_output('nested_expected_plain.txt')
-    assert generate_diff(path1, path2, formatter='plain') == expected
-
-
-# Formatter: Json
-def test_nested_json_json_formatter():
-    path1 = get_fixture_path('nested1.json')
-    path2 = get_fixture_path('nested2.json')
-    expected = get_expected_output('nested_expected_json.txt')
-    assert generate_diff(path1, path2, formatter='json') == expected
-
-
-def test_nested_yaml_json_formatter():
-    path1 = get_fixture_path('nested1.yaml')
-    path2 = get_fixture_path('nested2.yaml')
-    expected = get_expected_output('nested_expected_json.txt')
-    assert generate_diff(path1, path2, formatter='json') == expected
