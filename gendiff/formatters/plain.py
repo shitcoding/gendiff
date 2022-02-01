0ea4e1ca5bacaaa9392a01f593de0b9dcbcae218
shitcoding <shitcoder@pm.me>
2022-02-01 16:35:33 +0300

    refactor plain formatter

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -2,9 +2,9 @@
 
 
 def format_value(value):
-    """Formats value for plain formatter output.
+    """Formats value for plain output format.
     - If value is a string, return 'value'.
-    - If value is a special json specific, change the name from
+    - If value is a special Python specific, change the name from
     Python specific to json specific.
     - If value is nested (dict): return [complex value]
     - Otherwise return value as it is.
@@ -59,39 +59,3 @@ def format_diff(diff):
         return diff
 
     return walk_diff(diff)
-
-
-'''
-def format_diff(diff, ancestry=""):
-    """Returns formatted diff string in plain format.
-    Format:
-        - Property 'parent1.parent2.key' was added with value: 'value'
-        - Property 'parent1.parent2.key' was updated. \
-          From 'old_value' to 'new_value'
-        - Property 'parent1.parent2.key' was removed.
-    """
-    diff_strings = []
-    for entry in sorted(diff.items()):
-        key, (status, value) = entry
-        key = ancestry + key
-        if status == "removed":
-            diff_strings.append("Property '{0}' was removed".format(key))
-        elif status == "added":
-            diff_strings.append(
-                "Property '{0}' was {1} with value: {2}".format(
-                    key, status, format_value(value)
-                )
-            )
-        elif status == "changed":
-            old_value, new_value = value[0], value[1]
-            diff_strings.append(
-                "Property '{0}' was updated. ".format(key)
-                + "From {1} to {1}".format(
-                    format_value(old_value), format_value(new_value)
-                )
-            )
-        elif status == "nested":
-            diff_strings.append(format_diff(value, key + "."))
-    diff = "\n".join(diff_strings)
-    return rename_spec_values(diff)
-'''
